6945424b6b006a23b9ae4562298d5971f8f87f85
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-09-19 10:49:41 -0500

    Removed the threading and importing of the storm.py script

--- FA11y.py
+++ FA11y.py
@@ -1,6 +1,5 @@
 import threading
 from lib.icon import start_icon_detection
-from lib.storm import start_storm_detection
 from lib.hsr import start_health_shield_rarity_detection
 
 def main():
@@ -8,11 +7,6 @@ def main():
     icon_thread = threading.Thread(target=start_icon_detection)
     icon_thread.daemon = True
     icon_thread.start()
-
-    # Start the storm detection function in a separate thread
-    storm_thread = threading.Thread(target=start_storm_detection)
-    storm_thread.daemon = True
-    storm_thread.start()
 
     # Start the health, shield, and rarity detection function in a separate thread
     hsr_thread = threading.Thread(target=start_health_shield_rarity_detection)
