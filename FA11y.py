76f60bd387c6ac6fea8e74f50ff2076aef71af6a
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-09-23 07:32:44 -0500

    Added the importing and creation of a new thread for the mouse.py file

--- FA11y.py
+++ FA11y.py
@@ -1,6 +1,7 @@
 import threading
 from lib.icon import start_icon_detection
 from lib.hsr import start_health_shield_rarity_detection
+from lib.mouse import mouse_movement
 
 def main():
     # Start the icon detection function in a separate thread
@@ -13,7 +14,12 @@ def main():
     hsr_thread.daemon = True
     hsr_thread.start()
 
-    print("Icon, storm, and health/shield/rarity detection are now running in the background. Press any key followed by Enter in this window to exit.")
+    # Start the mouse movement in a seperate thread
+    mouse_thread = threading.Thread(target=mouse_movement)
+    mouse_thread.daemon = True
+    mouse_thread.start()
+
+    print("Mouse Movement, Player Icon, storm, and health/shield/rarity detection are now running in the background. Press any key followed by Enter in this window to exit.")
 
     input()  # Wait for user input to exit the program
 
