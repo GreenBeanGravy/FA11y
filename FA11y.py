cc09438ef9e396344b3b13ab76830f26dda8beb1
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-11-14 13:44:19 -0600

    Read Extended Desc
    Introduced a 'config.txt' file, currently used to toggle Mouse Keys, but will be used in the future for more settings.
    Made the message for which modules are running dynamic.
    Made the message for how to close the script make more sense.

--- FA11y.py
+++ FA11y.py
@@ -3,23 +3,43 @@ from lib.icon import start_icon_detection
 from lib.hsr import start_health_shield_rarity_detection
 from lib.mouse import mouse_movement
 
+def read_config():
+    with open('config.txt', 'r') as file:
+        for line in file:
+            if 'MouseKeys' in line:
+                # Check for 'true' regardless of case, default to 'false' otherwise
+                return line.split('=')[1].strip().lower() == 'true'
+    return False  # Default to False if not found or unknown value
+
 def main():
+    active_threads = []
+
     # Start the icon detection function in a separate thread
     icon_thread = threading.Thread(target=start_icon_detection)
     icon_thread.daemon = True
     icon_thread.start()
+    active_threads.append("Player Icon")
 
     # Start the health, shield, and rarity detection function in a separate thread
     hsr_thread = threading.Thread(target=start_health_shield_rarity_detection)
     hsr_thread.daemon = True
     hsr_thread.start()
+    active_threads.append("Health/Shield/Rarity detection")
 
-    # Start the mouse movement in a seperate thread
-    mouse_thread = threading.Thread(target=mouse_movement)
-    mouse_thread.daemon = True
-    mouse_thread.start()
+    # Check config and start the mouse movement if enabled
+    if read_config():
+        mouse_thread = threading.Thread(target=mouse_movement)
+        mouse_thread.daemon = True
+        mouse_thread.start()
+        active_threads.append("Mouse Movement")
 
-    print("Mouse Movement, Player Icon, storm, and health/shield/rarity detection are now running in the background. Press Enter in this window to exit.")
+    # Formatting the active threads list with 'and' before the last item
+    if len(active_threads) > 1:
+        active_thread_list = ', '.join(active_threads[:-1]) + ', and ' + active_threads[-1]
+    else:
+        active_thread_list = active_threads[0]
+
+    print(f"{active_thread_list} are now running in the background. Press Enter in this window to exit.")
 
     input()  # Wait for user input to exit the program
 
