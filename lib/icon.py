20073aae26628c0ee72b7349abc0ff9dc5b6a316
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-12-03 17:33:11 -0600

    Update icon.py

--- lib/icon.py
+++ lib/icon.py
@@ -1,7 +1,7 @@
-import cv2, numpy as np, pyautogui, ctypes, threading, time
+import cv2, numpy as np, pyautogui, ctypes, threading, time, win32api, configparser
 from accessible_output2.outputs.auto import Auto
 from lib.storm import start_storm_detection
-import configparser
+import lib.guis.gui as gui
 
 pyautogui.FAILSAFE = False
 
@@ -11,6 +11,7 @@ speaker = Auto()
 min_shape_size, max_shape_size = 1200, 2200
 roi_start_orig, roi_end_orig = (590, 190), (1490, 1010)
 VK_GRAVE_ACCENT = 0xC0
+VK_SHIFT = 0x10
 grave_accent_key_down = False
 
 def load_config():
@@ -43,6 +44,25 @@ def check_and_toggle_key(key, key_down, action):
     return current_state
 
 def icon_detection_cycle(selected_poi):
+    # Handling when SHIFT is pressed for custom GUI creation
+    if win32api.GetAsyncKeyState(VK_SHIFT) < 0:
+        screenshot = np.array(pyautogui.screenshot())
+        screenshot = cv2.resize(screenshot, None, fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
+        roi_color = screenshot[4 * roi_start_orig[1]:4 * roi_end_orig[1], 4 * roi_start_orig[0]:4 * roi_end_orig[0]]
+
+        roi_gray = cv2.cvtColor(roi_color, cv2.COLOR_BGR2GRAY)
+        _, binary_image = cv2.threshold(roi_gray, 229, 255, cv2.THRESH_BINARY)
+        valid_contours = find_valid_contours(binary_image)
+
+        if valid_contours:
+            largest_contour = max(valid_contours, key=cv2.contourArea)
+            center_mass_screen = process_contour(largest_contour, roi_color, None, None)  # Passing None as these are not used in this scenario
+            gui.create_gui(f"{center_mass_screen[0]},{center_mass_screen[1]}")
+        else:
+            speaker.speak("Player icon not located.")
+        return
+
+    # Rest of the function for normal operation
     if selected_poi[0].lower() == 'none':
         speaker.speak("No POI selected. Please update the config file.")
         return
@@ -95,49 +115,53 @@ def process_contour(contour, roi_color, selected_coordinates, selected_poi):
     center_mass_screen = ((center_mass[0] // 4) + roi_start_orig[0], (center_mass[1] // 4) + roi_start_orig[1])
     hull = cv2.convexHull(contour)
 
-    if len(hull) > 2:
-        vertices = np.squeeze(hull)
-        distances = [np.linalg.norm(v - center_mass) for v in vertices]
-        farthest_vertex = vertices[np.argmax(distances)]
-        direction_vector = farthest_vertex - np.array(center_mass)
 
-        # Corrected calculation for the bearing of the contour
-        direction_degree = (450 - np.degrees(np.arctan2(direction_vector[1], direction_vector[0]))) % 360
-
-        # Corrected calculation for the bearing from the contour to the POI
-        dx = selected_coordinates[0] - center_mass_screen[0]
-        dy = selected_coordinates[1] - center_mass_screen[1]
-        poi_degree = (450 - np.degrees(np.arctan2(dy, dx))) % 360
-
-        relative_position_vector = np.array(selected_coordinates) - np.array(center_mass_screen)
-        relative_position_to_poi = get_relative_direction(relative_position_vector, direction_degree)
-        min_distance = calculate_distance(center_mass_screen, selected_coordinates)
-        announce_position(selected_poi, relative_position_to_poi, min_distance)
-
-        # Drawing the contour
-        cv2.drawContours(roi_color, [contour], -1, (0, 255, 0), 3)
-
-        # Drawing the center of mass
-        cv2.circle(roi_color, center_mass, 5, (255, 0, 0), -1)
-
-        # Drawing the farthest vertex
-        cv2.circle(roi_color, tuple(farthest_vertex), 5, (0, 0, 255), -1)
-
-        # Drawing a line from the center to the farthest vertex
-        cv2.line(roi_color, center_mass, tuple(farthest_vertex), (255, 255, 0), 2)
-
-        # Marking the selected POI and drawing a line from vertex to POI
-        selected_poi_screen = (int(selected_coordinates[0]), int(selected_coordinates[1]))
-        cv2.circle(roi_color, selected_poi_screen, 5, (0, 255, 255), -1)
-        cv2.line(roi_color, tuple(farthest_vertex), selected_poi_screen, (0, 255, 255), 2)
+    if selected_coordinates is not None:
+        if len(hull) > 2:
+            vertices = np.squeeze(hull)
+            distances = [np.linalg.norm(v - center_mass) for v in vertices]
+            farthest_vertex = vertices[np.argmax(distances)]
+            direction_vector = farthest_vertex - np.array(center_mass)
     
-        # Displaying degrees as text
-        cv2.putText(roi_color, f"Contour Direction: {direction_degree:.2f} degrees", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        cv2.putText(roi_color, f"POI Direction: {poi_degree:.2f} degrees", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            # Corrected calculation for the bearing of the contour
+            direction_degree = (450 - np.degrees(np.arctan2(direction_vector[1], direction_vector[0]))) % 360
     
-        # Remove the comment below to enable debug screenshots
-        # cv2.imwrite('debug.png', roi_color)
+            # Corrected calculation for the bearing from the contour to the POI
+            dx = selected_coordinates[0] - center_mass_screen[0]
+            dy = selected_coordinates[1] - center_mass_screen[1]
+            poi_degree = (450 - np.degrees(np.arctan2(dy, dx))) % 360
+    
+            relative_position_vector = np.array(selected_coordinates) - np.array(center_mass_screen)
+            relative_position_to_poi = get_relative_direction(relative_position_vector, direction_degree)
+            min_distance = calculate_distance(center_mass_screen, selected_coordinates)
+            announce_position(selected_poi, relative_position_to_poi, min_distance)
+    
+            # Drawing the contour
+            cv2.drawContours(roi_color, [contour], -1, (0, 255, 0), 3)
+    
+            # Drawing the center of mass
+            cv2.circle(roi_color, center_mass, 5, (255, 0, 0), -1)
+    
+            # Drawing the farthest vertex
+            cv2.circle(roi_color, tuple(farthest_vertex), 5, (0, 0, 255), -1)
+    
+            # Drawing a line from the center to the farthest vertex
+            cv2.line(roi_color, center_mass, tuple(farthest_vertex), (255, 255, 0), 2)
+    
+            # Marking the selected POI and drawing a line from vertex to POI
+            selected_poi_screen = (int(selected_coordinates[0]), int(selected_coordinates[1]))
+            cv2.circle(roi_color, selected_poi_screen, 5, (0, 255, 255), -1)
+            cv2.line(roi_color, tuple(farthest_vertex), selected_poi_screen, (0, 255, 255), 2)
+        
+            # Displaying degrees as text
+            cv2.putText(roi_color, f"Contour Direction: {direction_degree:.2f} degrees", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(roi_color, f"POI Direction: {poi_degree:.2f} degrees", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        
+            # Remove the comment below to enable debug screenshots
+            # cv2.imwrite('debug.png', roi_color)
 
+    return center_mass_screen
+    
 def announce_position(selected_poi, relative_position_to_poi, distance):
     poi_name = selected_poi[0]
     if distance <= 40:
