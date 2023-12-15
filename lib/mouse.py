0ad343efd3406a7198493a164b3c35a939ef9a3a
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-12-15 11:11:39 -0600

    Now includes left and right click functions with LCTRL and RCTRL

--- lib/mouse.py
+++ lib/mouse.py
@@ -3,6 +3,10 @@ import time
 import threading
 
 # Constants
+VK_NUMLOCK = 0x90
+VK_LCONTROL, VK_RCONTROL = 0xA2, 0xA3
+MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP = 0x0002, 0x0004
+MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP = 0x0008, 0x0010
 VK_NUMPAD1, VK_NUMPAD3, VK_NUMPAD5, VK_NUMPAD7, VK_NUMPAD9 = 0x61, 0x63, 0x65, 0x67, 0x69
 VK_NUMPAD2, VK_NUMPAD8, VK_NUMPAD0, VK_NUMPAD4, VK_NUMPAD6 = 0x62, 0x68, 0x60, 0x64, 0x66
 INPUT_MOUSE, MOUSEEVENTF_MOVE, MOUSEEVENTF_WHEEL = 0, 0x0001, 0x0800
@@ -41,10 +45,32 @@ def mouse_scroll(amount):
     x = INPUT(type=INPUT_MOUSE, ii=MOUSEINPUT(dx=0, dy=0, mouseData=amount, dwFlags=MOUSEEVENTF_WHEEL, time=0, dwExtraInfo=None))
     ctypes.windll.user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
 
+def left_mouse_down():
+    down = INPUT(type=INPUT_MOUSE, ii=MOUSEINPUT(dx=0, dy=0, mouseData=0, dwFlags=MOUSEEVENTF_LEFTDOWN, time=0, dwExtraInfo=None))
+    ctypes.windll.user32.SendInput(1, ctypes.byref(down), ctypes.sizeof(down))
+
+def left_mouse_up():
+    up = INPUT(type=INPUT_MOUSE, ii=MOUSEINPUT(dx=0, dy=0, mouseData=0, dwFlags=MOUSEEVENTF_LEFTUP, time=0, dwExtraInfo=None))
+    ctypes.windll.user32.SendInput(1, ctypes.byref(up), ctypes.sizeof(up))
+
+def right_mouse_down():
+    down = INPUT(type=INPUT_MOUSE, ii=MOUSEINPUT(dx=0, dy=0, mouseData=0, dwFlags=MOUSEEVENTF_RIGHTDOWN, time=0, dwExtraInfo=None))
+    ctypes.windll.user32.SendInput(1, ctypes.byref(down), ctypes.sizeof(down))
+
+def right_mouse_up():
+    up = INPUT(type=INPUT_MOUSE, ii=MOUSEINPUT(dx=0, dy=0, mouseData=0, dwFlags=MOUSEEVENTF_RIGHTUP, time=0, dwExtraInfo=None))
+    ctypes.windll.user32.SendInput(1, ctypes.byref(up), ctypes.sizeof(up))
+
+def is_numlock_on():
+    return ctypes.windll.user32.GetKeyState(VK_NUMLOCK) & 1
+
 def mouse_movement():
     numpad_keys_down = {key: False for key in [VK_NUMPAD1, VK_NUMPAD3, VK_NUMPAD5, VK_NUMPAD7, VK_NUMPAD9, VK_NUMPAD2, VK_NUMPAD8, VK_NUMPAD0, VK_NUMPAD4, VK_NUMPAD6]}
+    control_keys_down = {key: False for key in [VK_LCONTROL, VK_RCONTROL]}
 
     while True:
+        numlock_on = is_numlock_on()
+
         for key in numpad_keys_down:
             key_current_state = bool(ctypes.windll.user32.GetAsyncKeyState(key))
             if key_current_state and not numpad_keys_down[key]:
@@ -70,5 +96,20 @@ def mouse_movement():
                     smooth_move_mouse(-MOUSE_SENSITIVITY, 0, SMOOTH_MOVE_DURATION)
                 elif key == VK_NUMPAD6:
                     smooth_move_mouse(MOUSE_SENSITIVITY, 0, SMOOTH_MOVE_DURATION)
-
             numpad_keys_down[key] = key_current_state
+
+        if numlock_on:
+            for key in control_keys_down:
+                key_current_state = bool(ctypes.windll.user32.GetAsyncKeyState(key))
+                if key_current_state != control_keys_down[key]:  # Key state changed
+                    if key == VK_LCONTROL:
+                        if key_current_state:
+                            left_mouse_down()
+                        else:
+                            left_mouse_up()
+                    elif key == VK_RCONTROL:
+                        if key_current_state:
+                            right_mouse_down()
+                        else:
+                            right_mouse_up()
+                control_keys_down[key] = key_current_state
