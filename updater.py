6eccdfa140b9875a343b8ff52bf06d1a40020ced
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-12-21 12:44:09 -0600

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,6 +1,15 @@
 import subprocess
 import os
 import sys
+import requests
+from accessible_output2.outputs.auto import Auto
+
+def check_python_version():
+    if sys.version_info.major == 3 and sys.version_info.minor == 9:
+        return True
+    else:
+        print(f"Python 3.9 is not in use. You are using Python {sys.version_info.major}.{sys.version_info.minor}.")
+        return False
 
 def install_required_modules():
     print("Checking for required modules...")
@@ -8,10 +17,10 @@ def install_required_modules():
     subprocess.run([sys.executable, '-m', 'pip', 'install'] + modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("Required modules installed!")
 
-install_required_modules()
+if not check_python_version():
+    sys.exit(1)
 
-import requests
-from accessible_output2.outputs.auto import Auto
+install_required_modules()
 
 speaker = Auto()
 
@@ -23,12 +32,15 @@ def get_repo_files(repo, branch='main'):
         return [item['path'] for item in tree if item['type'] == 'blob']
     return []
 
-def download_file(repo, file_path, is_readme=False):
+def download_file(repo, file_path):
     url = f"https://raw.githubusercontent.com/{repo}/main/{file_path}"
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text if is_readme else response.content
-    return None
+    return response.content if response.status_code == 200 else None
+
+def file_needs_update(local_path, github_content):
+    if not os.path.exists(local_path) or open(local_path, 'rb').read() != github_content:
+        return True
+    return False
 
 def check_for_updates(repo, script_name):
     repo_files = get_repo_files(repo)
@@ -36,15 +48,15 @@ def check_for_updates(repo, script_name):
 
     for file_path in repo_files:
         if file_path.lower() == 'readme.md':
-            readme_content = download_file(repo, file_path, is_readme=True)
-            if readme_content:
-                with open('README.txt', 'w', encoding='utf-8') as file:
+            readme_content = download_file(repo, file_path)
+            if readme_content and file_needs_update('README.txt', readme_content):
+                with open('README.txt', 'wb') as file:
                     file.write(readme_content)
                 print("README updated.")
             continue
 
         if file_path.endswith(('.py', '.txt', '.png')):
-            if file_path == 'CUSTOM_POI.txt' and os.path.exists(file_path):
+            if file_path == 'config.txt' and os.path.exists(file_path):
                 continue
 
             if file_path.endswith(script_name):
@@ -55,21 +67,10 @@ def check_for_updates(repo, script_name):
                 print(f"Failed to download {file_path} from GitHub.")
                 continue
 
-            directory_name = os.path.dirname(file_path)
-            if directory_name and not os.path.exists(directory_name):
-                os.makedirs(directory_name, exist_ok=True)
-
-            if not os.path.exists(file_path) or open(file_path, 'rb').read() != github_content:
+            if file_needs_update(file_path, github_content):
                 updates_available = True
 
-    if updates_available:
-        speaker.speak("Updates available.")
-        update_mode = input("Press Enter to update all files automatically or type 'manual' to select updates manually: ").strip().lower()
-        speaker.speak("Processing updates, please wait.")
-        process_updates(repo, repo_files, update_mode, script_name)
-    else:
-        print("You are on the latest version!")
-        speaker.speak("You are on the latest version!")
+    return updates_available
 
 def process_updates(repo, repo_files, update_mode, script_name):
     for file_path in repo_files:
@@ -77,11 +78,11 @@ def process_updates(repo, repo_files, update_mode, script_name):
             continue  # Skip README.md and the script itself
 
         if file_path.endswith(('.py', '.txt', '.png')):
-            if file_path == 'CUSTOM_POI.txt' and os.path.exists(file_path):
+            if file_path == 'config.txt' and os.path.exists(file_path):
                 continue
 
             github_content = download_file(repo, file_path)
-            if github_content is None:
+            if github_content is None or not file_needs_update(file_path, github_content):
                 continue
 
             if update_mode != 'manual':
@@ -97,29 +98,23 @@ def process_updates(repo, repo_files, update_mode, script_name):
                 else:
                     print(f"Update for {file_path} skipped.")
 
-def check_python_version():
-    if sys.version_info.major == 3 and sys.version_info.minor == 9:
-        return True
-    else:
-        print(f"Python 3.9 is not in use, you are using Python {sys.version_info.major}.{sys.version_info.minor}. Accessible Output 2 requires Python 3.9.x to function properly. Press Enter to install anyways.")
-        input()
-        return False
-
 def main():
     script_name = os.path.basename(__file__)  # Get the name of the current script
     check_python_version()
 
-    check_for_updates("GreenBeanGravy/FA11y", script_name)
-    speaker.speak("Update check complete.")
+    if check_for_updates("GreenBeanGravy/FA11y", script_name):
+        update_mode = input("Updates available. Press Enter to update all files automatically or type 'manual' to select updates manually: ").strip().lower()
+        process_updates("GreenBeanGravy/FA11y", get_repo_files("GreenBeanGravy/FA11y"), update_mode, script_name)
+        speaker.speak("Updates processed.")
+    else:
+        print("You are on the latest version!")
+        speaker.speak("You are on the latest version!")
 
     if os.path.exists('requirements.txt'):
         print("Installing packages from requirements.txt...")
         subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("All updates applied!")
         speaker.speak("All updates applied!")
-
-    else:
-        print("requirements.txt not found.")
 
 if __name__ == "__main__":
     main()
