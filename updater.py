e1f70ba4b7e6097ad72c1e33857f4ccee855ad77
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-12-20 22:50:43 -0600

    Update updater.py

--- updater.py
+++ updater.py
@@ -23,20 +23,26 @@ def get_repo_files(repo, branch='main'):
         return [item['path'] for item in tree if item['type'] == 'blob']
     return []
 
-def download_file(repo, file_path):
+def download_file(repo, file_path, is_readme=False):
     url = f"https://raw.githubusercontent.com/{repo}/main/{file_path}"
     response = requests.get(url)
-    return response.content if response.status_code == 200 else None
+    if response.status_code == 200:
+        return response.text if is_readme else response.content
+    return None
 
 def check_for_updates(repo, script_name):
     repo_files = get_repo_files(repo)
     updates_available = False
 
-    speaker.speak("Checking for updates.")
-    update_mode = input("Press Enter to update all files automatically or type 'manual' to select updates manually: ").strip().lower()
-    speaker.speak("Processing updates, please wait.")
-
     for file_path in repo_files:
+        if file_path.lower() == 'readme.md':
+            readme_content = download_file(repo, file_path, is_readme=True)
+            if readme_content:
+                with open('README.txt', 'w', encoding='utf-8') as file:
+                    file.write(readme_content)
+                print("README updated.")
+            continue
+
         if file_path.endswith(('.py', '.txt', '.png')):
             if file_path == 'CUSTOM_POI.txt' and os.path.exists(file_path):
                 continue
@@ -53,32 +59,43 @@ def check_for_updates(repo, script_name):
             if directory_name and not os.path.exists(directory_name):
                 os.makedirs(directory_name, exist_ok=True)
 
-            if not os.path.exists(file_path):
-                with open(file_path, 'wb') as file:
-                    file.write(github_content)
-                print(f"New file {file_path} has been downloaded.")
+            if not os.path.exists(file_path) or open(file_path, 'rb').read() != github_content:
                 updates_available = True
-            else:
-                with open(file_path, 'rb') as file:
-                    local_content = file.read()
-                if local_content != github_content:
-                    updates_available = True
-                    if update_mode != 'manual':
-                        with open(file_path, 'wb') as file:
-                            file.write(github_content)
-                        print(f"Updated {file_path}")
-                    else:
-                        choice = input(f"Update available for {file_path}. Do you want to update? (Y/N): ").strip().lower()
-                        if choice == 'y':
-                            with open(file_path, 'wb') as file:
-                                file.write(github_content)
-                            print(f"Updated {file_path}")
-                        else:
-                            print(f"Update for {file_path} skipped.")
 
-    if not updates_available:
+    if updates_available:
+        speaker.speak("Updates available.")
+        update_mode = input("Press Enter to update all files automatically or type 'manual' to select updates manually: ").strip().lower()
+        speaker.speak("Processing updates, please wait.")
+        process_updates(repo, repo_files, update_mode, script_name)
+    else:
         print("You are on the latest version!")
         speaker.speak("You are on the latest version!")
+
+def process_updates(repo, repo_files, update_mode, script_name):
+    for file_path in repo_files:
+        if file_path.lower() == 'readme.md' or file_path.endswith(script_name):
+            continue  # Skip README.md and the script itself
+
+        if file_path.endswith(('.py', '.txt', '.png')):
+            if file_path == 'CUSTOM_POI.txt' and os.path.exists(file_path):
+                continue
+
+            github_content = download_file(repo, file_path)
+            if github_content is None:
+                continue
+
+            if update_mode != 'manual':
+                with open(file_path, 'wb') as file:
+                    file.write(github_content)
+                print(f"Updated {file_path}")
+            else:
+                choice = input(f"Update available for {file_path}. Do you want to update? (Y/N): ").strip().lower()
+                if choice == 'y':
+                    with open(file_path, 'wb') as file:
+                        file.write(github_content)
+                    print(f"Updated {file_path}")
+                else:
+                    print(f"Update for {file_path} skipped.")
 
 def check_python_version():
     if sys.version_info.major == 3 and sys.version_info.minor == 9:
@@ -98,9 +115,8 @@ def main():
     if os.path.exists('requirements.txt'):
         print("Installing packages from requirements.txt...")
         subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("All updates applied! Press Enter to close the updater.")
-        speaker.speak("All updates applied! Press Enter to close the updater.")
-        input()
+        print("All updates applied!")
+        speaker.speak("All updates applied!")
 
     else:
         print("requirements.txt not found.")
