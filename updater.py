4e503ee5cda15179287ed4cce88326ac16f759dd
GreenBeanGravy <73409904+GreenBeanGravy@users.noreply.github.com>
2023-12-21 12:46:32 -0600

    Update updater.py

--- updater.py
+++ updater.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import sys
 import requests
+import shutil
 from accessible_output2.outputs.auto import Auto
 
 def check_python_version():
@@ -11,14 +12,14 @@ def check_python_version():
         print(f"Python 3.9 is not in use. You are using Python {sys.version_info.major}.{sys.version_info.minor}.")
         return False
 
+if not check_python_version():
+    sys.exit(1)
+
 def install_required_modules():
     print("Checking for required modules...")
     modules = ['requests', 'accessible_output2']
     subprocess.run([sys.executable, '-m', 'pip', 'install'] + modules, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("Required modules installed!")
-
-if not check_python_version():
-    sys.exit(1)
 
 install_required_modules()
 
@@ -45,6 +46,7 @@ def file_needs_update(local_path, github_content):
 def check_for_updates(repo, script_name):
     repo_files = get_repo_files(repo)
     updates_available = False
+    script_updated = False
 
     for file_path in repo_files:
         if file_path.lower() == 'readme.md':
@@ -59,9 +61,6 @@ def check_for_updates(repo, script_name):
             if file_path == 'config.txt' and os.path.exists(file_path):
                 continue
 
-            if file_path.endswith(script_name):
-                continue  # Skip updating the script itself
-
             github_content = download_file(repo, file_path)
             if github_content is None:
                 print(f"Failed to download {file_path} from GitHub.")
@@ -69,13 +68,15 @@ def check_for_updates(repo, script_name):
 
             if file_needs_update(file_path, github_content):
                 updates_available = True
+                if file_path.endswith(script_name):
+                    script_updated = True
 
-    return updates_available
+    return updates_available, script_updated
 
 def process_updates(repo, repo_files, update_mode, script_name):
     for file_path in repo_files:
-        if file_path.lower() == 'readme.md' or file_path.endswith(script_name):
-            continue  # Skip README.md and the script itself
+        if file_path.lower() == 'readme.md':
+            continue  # Skip README.md
 
         if file_path.endswith(('.py', '.txt', '.png')):
             if file_path == 'config.txt' and os.path.exists(file_path):
@@ -85,26 +86,45 @@ def process_updates(repo, repo_files, update_mode, script_name):
             if github_content is None or not file_needs_update(file_path, github_content):
                 continue
 
-            if update_mode != 'manual':
-                with open(file_path, 'wb') as file:
-                    file.write(github_content)
+            if update_mode != 'manual' or (update_mode == 'manual' and user_confirms_update(file_path)):
+                apply_update(file_path, github_content)
                 print(f"Updated {file_path}")
-            else:
-                choice = input(f"Update available for {file_path}. Do you want to update? (Y/N): ").strip().lower()
-                if choice == 'y':
-                    with open(file_path, 'wb') as file:
-                        file.write(github_content)
-                    print(f"Updated {file_path}")
-                else:
-                    print(f"Update for {file_path} skipped.")
+
+def user_confirms_update(file_path):
+    choice = input(f"Update available for {file_path}. Update file? (Y/N): ").strip().lower()
+    return choice == 'y'
+
+def apply_update(file_path, github_content):
+    with open(file_path, 'wb') as file:
+        file.write(github_content)
+
+def update_self_and_restart(repo, script_name):
+    github_content = download_file(repo, script_name)
+    if github_content is None:
+        return
+
+    temp_file = script_name + '.temp'
+    with open(temp_file, 'wb') as file:
+        file.write(github_content)
+
+    os.replace(temp_file, script_name)
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
 
 def main():
-    script_name = os.path.basename(__file__)  # Get the name of the current script
+    script_name = os.path.basename(__file__)
     check_python_version()
 
-    if check_for_updates("GreenBeanGravy/FA11y", script_name):
+    updates_available, script_updated = check_for_updates("GreenBeanGravy/FA11y", script_name)
+
+    if updates_available:
         update_mode = input("Updates available. Press Enter to update all files automatically or type 'manual' to select updates manually: ").strip().lower()
         process_updates("GreenBeanGravy/FA11y", get_repo_files("GreenBeanGravy/FA11y"), update_mode, script_name)
+
+        if script_updated:
+            update_self_and_restart("GreenBeanGravy/FA11y", script_name)
+            return  # The script will restart after this line
+
         speaker.speak("Updates processed.")
     else:
         print("You are on the latest version!")
